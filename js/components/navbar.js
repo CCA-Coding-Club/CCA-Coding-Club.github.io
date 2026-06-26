923ff1811eb525826a77eea41d539dd9b72b203e
westonmook <westonmook@gmail.com>
2026-06-26 14:47:11 -0600

    fix 2

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -44,14 +44,15 @@ document.addEventListener("DOMContentLoaded", function () {
                '<button id="github-login-btn">Authenticate via GitHub</button>' +
                '<button id="logout-btn" style="display: none;">Logout</button>' +
                '<div id="user-telemetry"></div>' +
+               '<script type="module" src="./js/base/auth-logic.js"></script>' +
                '</div>';
-  var authlogic = '<script type="module" src="./js/base/auth-logic.js"></script>'
+  //var authlogic = '<script type="module" src="./js/base/auth-logic.js"></script>'
   // Inject the full navbar
   //weston: I added authHtml to navbar links
   container.innerHTML =
     '<nav class="navbar"><div class="navbar__inner">' +
     '<a href="' + prefix + 'index.html" class="navbar__brand">' +
     '<img src="' + prefix + 'assets/logo.png" class="navbar__logo" alt="Coding Club Logo" />Coding Club</a>' +
-    '<div class="navbar__links">' + linksHtml + '</div>' + '<div class="navbar__links" "navbar__profile">' + authHtml + authlogic + '</div>' +
+    '<div class="navbar__links">' + linksHtml + '</div>' + '<div class="navbar__links" "navbar__profile">' + authHtml + '</div>' +
     '</div></nav>';
 });
